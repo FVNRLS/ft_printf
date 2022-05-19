b7b6d96a88d99e2c353936e72f15555f2b0051e0
amenra <amenra@libre>
2022-05-19 14:14:25 +0200

    del unused

--- ft_check_mods.c
+++ ft_check_mods.c
@@ -6,7 +6,7 @@
 /*   By: rmazurit <rmazurit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 11:12:19 by rmazurit          #+#    #+#             */
-/*   Updated: 2022/05/17 21:14:03 by rmazurit         ###   ########.fr       */
+/*   Updated: 2022/05/19 14:13:47 by rmazurit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,14 @@
 /*
 	Order prototype:
 	%[flags][width][.precision][.length]specifier
+	Check all format modifiers and set the flag to 1 if true.
+	Check the precision and convert numbers after '.' sign to precision pads 
+	and normal pads.
 */
 
 void	ft_check_mods(t_input *input, t_mod *mods)
 {
 	mods->spec = input->format[input->pos];
-	//firstly check the flags
-
-	//check if form mod is zero and count the number of pads
-	// if the previous element isn't a digit ten 0 is a flag - otherwise its a 
 	if (mods->spec == '0' && (ft_isdigit(input->format[input->pos - 1]) == 0) 
 		&& ft_isdigit(input->format[input->pos + 1]) == 1)
 		mods->zero = 1;
@@ -35,8 +34,6 @@ void	ft_check_mods(t_input *input, t_mod *mods)
 		mods->space = 1;
 	if (mods->spec == '#')
 		mods->hash = 1;
-		
-	// //then check the width and precision in combination
 	if (ft_isdigit(mods->spec) == 1)
 	{
 		mods->width = 1;
@@ -44,7 +41,6 @@ void	ft_check_mods(t_input *input, t_mod *mods)
 			mods->pads *= 10;
 		mods->pads += (mods->spec - '0');
 	}
-	//then check the precision
 	if (mods->spec == '.')
 	{
 		mods->prec = 1;
@@ -52,5 +48,4 @@ void	ft_check_mods(t_input *input, t_mod *mods)
 		mods->width = 0;
 		mods->pads = 0;
 	}
-	//exceptions
 }
